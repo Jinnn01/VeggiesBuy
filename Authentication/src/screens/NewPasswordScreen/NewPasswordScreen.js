45cb2d17f420a1261f0726d17890e2bf8020f3ab
Sravya0303 <kotla.sravs1997@gmail.com>
2023-05-20 18:06:43 +1000

    added Authentication

---
+++ Authentication/src/screens/NewPasswordScreen/NewPasswordScreen.js
@@ -0,0 +1,98 @@
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, ScrollView, Alert} from 'react-native';
+import CustomInput from '../../components/CustomInput';
+import CustomButton from '../../components/CustomButton';
+import {useNavigation} from '@react-navigation/native';
+import {useForm} from 'react-hook-form';
+import {Auth} from 'aws-amplify';
+import Background from '../ScreenBackground/Background';
+
+const NewPasswordScreen = () => {
+  const {control, handleSubmit} = useForm();
+
+  const navigation = useNavigation();
+
+  const onSubmitPressed = async data => {
+    try {
+      await Auth.forgotPasswordSubmit(data.username, data.code, data.password);
+      navigation.navigate('SignIn');
+    } catch (e) {
+      Alert.alert('Oops', e.message);
+    }
+  };
+
+  const onSignInPress = () => {
+    navigation.navigate('SignIn');
+  };
+
+  return (
+    <Background>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.root}>
+        <Text style={styles.title}>Reset your password</Text>
+
+        <CustomInput
+          placeholder="Username"
+          name="username"
+          control={control}
+          rules={{required: 'Username is required'}}
+        />
+
+        <CustomInput
+          placeholder="Code"
+          name="code"
+          control={control}
+          rules={{required: 'Code is required'}}
+        />
+
+        <CustomInput
+          placeholder="Enter your new password"
+          name="password"
+          control={control}
+          secureTextEntry
+          rules={{
+            required: 'Password is required',
+            minLength: {
+              value: 8,
+              message: 'Password should be at least 8 characters long',
+            },
+          }}
+        />
+
+        <CustomButton text="Submit" onPress={handleSubmit(onSubmitPressed)} />
+
+        <CustomButton
+          text="Back to Sign in"
+          onPress={onSignInPress}
+          type="TERTIARY"
+        />
+      </View>
+    </ScrollView>
+    </Background>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+    margin: 10,
+    width: 400,
+    paddingHorizontal: 25,
+    paddingVertical:25,
+  },
+  text: {
+    color: 'gray',
+    marginVertical: 10,
+  },
+  link: {
+    color: '#FDB075',
+  },
+});
+
+export default NewPasswordScreen;
--- VeggiesBuy/ModelData.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ModelData.swift
-//  VeggiesBuy
-//
-//  Created by Gabriel Komarnicki on 12/5/2023.
-//
-
-import Foundation
