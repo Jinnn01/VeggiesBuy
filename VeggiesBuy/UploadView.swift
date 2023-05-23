74eb100dbab40fd3a79b2d45a09790a84cdfe75f
Jinnn01 <60898623+Jinnn01@users.noreply.github.com>
2023-05-23 11:25:17 +1000

    Update UploadView.swift

--- VeggiesBuy/UploadView.swift
+++ VeggiesBuy/UploadView.swift
@@ -41,8 +41,6 @@ class AddItemModel: ObservableObject {
                 print("Error: \(error)")
                 return
             }
-            
-            // Data sent successfully
         }
         
         task.resume()
@@ -114,13 +112,22 @@ struct UploadView: View {
     @State private var vegetablePrice = ""
     @State private var vegetableUnit = ""
     @State private var supermarketName = ""
-    //@State private var isShowingAlert = false
     @State private var isShowingConfirmation = false
     @State private var isShowingAlert = false
     
     // Document scanning
     @State private var scannedImage: UIImage?
     @State private var isShowingScanner = false
+    
+    @State private var supermarkets: [String] = []
+    @State private var selectedSupermarketName: String = ""
+    
+    init() {
+        fetchSupermarkets()
+        if let firstSupermarket = supermarkets.first {
+            selectedSupermarketName = firstSupermarket
+        }
+    }
     
     var body: some View {
         NavigationView {
@@ -135,12 +142,27 @@ struct UploadView: View {
                         Form {
                             Section(header: Text("Vegetable Details")) {
                                 TextField("Vegetable Name", text: $vegetableName)
+                                //TextField("Vegetable Price", text: formatCurrencyText($vegetablePrice))
+                                    //.keyboardType(.decimalPad)
                                 TextField("Vegetable Price", text: $vegetablePrice)
                                 TextField("Vegetable Unit", text: $vegetableUnit)
                             }
                             
                             Section(header: Text("Supermarket Details"), footer: Text("You can either manually enter the item details with the form above, or tap the Camera icon in the top-left corner to open the OCR scanner.")) {
-                                TextField("Supermarket Name", text: $supermarketName)
+                                Picker("Supermaket:", selection: $selectedSupermarketName) {
+                                    ForEach(supermarkets, id: \.self) { supermarket in
+                                        Text(supermarket)
+                                    }
+                                }
+                            }
+                            var supermarkets: [String] {
+                                get {
+                                    fetchSupermarkets()
+                                    return self.supermarkets
+                                }
+                                set {
+                                    self.supermarkets = newValue
+                                }
                             }
                         }
                     }
@@ -148,7 +170,7 @@ struct UploadView: View {
 
 
                     .sheet(isPresented: $isShowingScanner, onDismiss: {
-                        // Handle dismiss action if needed
+                        
                     }) {
                         DocumentScannerView(scannedImage: $scannedImage)
                     }
@@ -181,20 +203,70 @@ struct UploadView: View {
             Alert(title: Text("Success"), message: Text("Vegetable successfully submitted!"), dismissButton: .default(Text("OK")))
         }
     }
+    /*
+    func formatCurrencyText(_ text: Binding<String>) -> Binding<String> {
+        return Binding<String>(
+            get: { text.wrappedValue },
+            set: { newValue in
+                let filtered = newValue.filter { "0123456789.".contains($0) }
+                text.wrappedValue = filtered.isEmpty ? "" : "$" + filtered
+            }
+        )
+    }*/
+    
+    func fetchSupermarkets() {
+        guard let url = URL(string: "http://localhost:5001/item") else {
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }
+
+            if let data = data {
+                do {
+                    let vegetableUploads = try JSONDecoder().decode([VegetableUpload].self, from: data)
+                    var uniqueSupermarkets: [String] = []
+                    for vegetableUpload in vegetableUploads {
+                        if !uniqueSupermarkets.contains(vegetableUpload.sname) {
+                            uniqueSupermarkets.append(vegetableUpload.sname)
+                        }
+                    }
+                    uniqueSupermarkets.sort()
+                    DispatchQueue.main.async {
+                        self.supermarkets = uniqueSupermarkets
+                    }
+                } catch {
+                    print("Failed to decode vegetable uploads: \(error)")
+                }
+            }
+        }
+        task.resume()
+    }
+
     
     func saveVegItem() async {
-        let vegetable = VegetableUpload(
-            sname: supermarketName,
-            vname: vegetableName,
-            price: Float(vegetablePrice) ?? 0,
-            unit: vegetableUnit
-        )
-
-        let addItemModel = AddItemModel()
-        addItemModel.save(vegetable)
-        
-        isShowingAlert = true
+        if let price = Float(vegetablePrice) {
+            let roundedPrice = (price * 100).rounded() / 100
+            
+            let vegetable = VegetableUpload(
+                sname: selectedSupermarketName,
+                vname: vegetableName,
+                price: roundedPrice,
+                unit: vegetableUnit
+            )
+            
+            let addItemModel = AddItemModel()
+            addItemModel.save(vegetable)
+            
+            isShowingAlert = true
+        } else {
+            print("Invalid price input")
+        }
     }
+
 }
 
 
